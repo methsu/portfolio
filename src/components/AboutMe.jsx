8f3e7f1c690be3362c9a0dfdc8d3d8a09d418f3c
methsu <thiyaana.vidanaarachchi@gmail.com>
2025-06-25 19:18:05 +0530

    add linkdin link to let's talk

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -138,7 +138,7 @@ const AboutMe = () => {
                   Download CV
                 </a>
                 <a 
-                  href="https://github.com/methsu" 
+                  href="https://www.linkedin.com/in/thiyaana-vidanaarachchi/" 
                   className="inline-flex items-center bg-transparent border border-white/20 hover:border-yellow-400 text-white hover:text-yellow-400 px-6 py-3 rounded-lg font-medium transition-all duration-300"
                 >
                   <svg className="w-5 h-5 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
